81e25741b1f6a1d135e576be78e24424a95845af
kenne <kenneth.p.galvez@gmail.com>
2021-10-14 22:16:53 +0800

    Complete design of admin side

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.login, name='login'),
     path('homepage/', views.homepage, name='homepage'),
+    path('request/', views.request, name='request'),
+    path('clinic/', views.clinic, name='clinic'),
+    path('settings/', views.settings, name='settings'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -7,3 +7,12 @@ def login(request):
 def homepage(request):
     return render(request,'homepage.html')
 
+def request(request):
+    return render(request,'request.html')
+
+def clinic(request):
+    return render(request,'clinic.html')
+
+def settings(request):
+    return render(request,'settings.html')
+
