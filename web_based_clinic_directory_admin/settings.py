bad911c125d7dbb8666169231e265c9ea913bc17
mugiwara007 <centeno.johnisrael.j.1911@gmail.com>
2021-12-17 01:32:32 +0800

    add app password on email password

--- web_based_clinic_directory_admin/settings.py
+++ web_based_clinic_directory_admin/settings.py
@@ -133,5 +133,5 @@ EMAIL_HOST_USER = 'clinic.directory.2021@gmail.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = 'Clinic_2021'
+EMAIL_HOST_PASSWORD = 'jbczqrzqvafgmvxd'
 
